739a17e2bc75f995b680fe1d1e073d704245e59e
Jay Fault <alchzh@gmail.com>
2016-03-15 00:52:33 +0000

    new genimages

--- genimages.py
+++ genimages.py
@@ -3,5 +3,5 @@ Run from inside root folder of repo"""
 import os
 with open('images.md', 'w') as f:
     for image in sorted(os.listdir("Images")):
-        f.write("%s\n\n" % image)
+        f.write("[{0}](Schematics/{0})\n\n".format(image.replace("png","schematic")))
         f.write("![{0}](Images/{0})\n\n".format(image))
