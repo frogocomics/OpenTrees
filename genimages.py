d9b88e4ffc1f06ebc12d4b26bc48228bc26265e5
Jeff Chen <frogocomics@gmail.com>
2016-03-14 20:07:04 -0700

    Update Python script to use thumbnails instead of full size images

--- genimages.py
+++ genimages.py
@@ -1,7 +1,9 @@
 """A small script for generating a markdown page from images.
 Run from inside root folder of repo"""
 import os
+os.remove("images.md")
 with open('images.md', 'w') as f:
-    for image in sorted(os.listdir("Images")):
+    print("Generating markdown")
+    for image in sorted(os.listdir("Images\\Thumbnails")):
         f.write("%s\n\n" % image)
         f.write("![{0}](Images/{0})\n\n".format(image))
