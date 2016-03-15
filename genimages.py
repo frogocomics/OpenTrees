7efeef271f985e8fdeae9591f10255c5b347ba7f
Jay Fault <alchzh@gmail.com>
2016-03-15 12:12:51 +0000

    New genimages

--- genimages.py
+++ genimages.py
@@ -3,12 +3,7 @@ Run from inside root folder of repo"""
 import os
 os.remove("images.md")
 with open('images.md', 'w') as f:
-<<<<<<< HEAD
     print("Generating markdown")
-    for image in sorted(os.listdir("Images\\Thumbnails")):
-        f.write("%s\n\n" % image)
-=======
-    for image in sorted(os.listdir("Images")):
+    for image in sorted(os.listdir("Images/Thumbnails")):
         f.write("[{0}](Schematics/{0})\n\n".format(image.replace("png","schematic")))
->>>>>>> origin/master
-        f.write("![{0}](Images/{0})\n\n".format(image))
+        f.write("![{0}](Images/Thumbnails/{0})\n\n".format(image))
